c15a5bf0eca43fabff0b11aa0916159f0e9bc178
bureado <j@bureado.com>
2014-01-18 08:37:40 +0000

    port changes

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var path = require('path'),
 app.use(express.static(__dirname + '/public'));
 
 var server = http.createServer(app);
-server.listen({port:process.env.PORT});
+server.listen(process.env.PORT);
+//server.listen(9999);
 
 var wss = new WebSocketServer({server: server});
 
